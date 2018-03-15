9a5b2784e3ef8cf3d248d72d90a54964b4a02bb3
Hirofumi Wakasugi <baenej@gmail.com>
2018-03-16 00:19:02 +0900

    Fix wrong style.css filename with digest

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const extractSass = new ExtractTextPlugin(
-  {
-    filename: "[name].[contenthash].css",
-    disable: process.env.NODE_ENV === "development"
-  }
-)
+const extractSass = new ExtractTextPlugin('stylesheets/style.css')
 
 module.exports = {
   entry: {
